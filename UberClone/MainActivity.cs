d589a7f056dc1ac7a308a77b59a2ce7542f6ec0b
Ricardo <rdkardo@gmail.com>
2020-11-16 19:31:46 -0600

    Agregar archivos de proyecto.

---
+++ UberClone/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace UberClone
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TextView txtNumber;
+            int number = 0;
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.textView1);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+            txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+            txtNumber.Text = (--number).ToString();
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
